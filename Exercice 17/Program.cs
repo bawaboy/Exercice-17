b4c9f7fd2c5cbd8cf943d3067646c7a502cb97c1
Abdou <Abdou@DESKTOP-Q7REKVT>
2024-02-22 15:23:12 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice 17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Saisie
+            Console.Write("Entrez un nombre :");
+            int nombre = int.Parse(Console.ReadLine());
+
+            Console.Write("Trouvez la racine carré du nombre saisie :");
+            int racine = int.Parse(Console.ReadLine());
+
+            if (racine == Math.Sqrt(nombre))
+            {
+                Console.WriteLine("BRAVO ! MAMAN A DIT MON FILS 'BRAVO'");
+            }
+            else
+            {
+                Console.WriteLine("NOPE ! La réponse est " + Math.Sqrt(nombre));
+            }
+            Console.ReadLine();
+        }
+    }
+}
